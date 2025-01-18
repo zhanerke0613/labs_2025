0f6c51025b97bd3ad6b53049d84d01b8aed49cf9
zhanerke <zhanerke0613@gmail.com>
2025-01-19 02:42:28 +0500

    data types

---
+++ lab1/6.py
@@ -0,0 +1,26 @@
+x = 5
+print(type(x))
+
+x = "Hello World"
+print(type(x))
+
+x = 20.5
+print(type(x))
+
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = None
+print(type(x))
+
+x = True
+print(type(x))
