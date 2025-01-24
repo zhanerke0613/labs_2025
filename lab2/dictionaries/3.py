b581d4257e3c6924203dddb620ac0dcb4cf8e569
zhanerke <zhanerke0613@gmail.com>
2025-01-24 14:26:41 +0500

    change items

---
+++ lab2/dictionaries/3.py
@@ -0,0 +1,16 @@
+#Change Dictionary Items
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
