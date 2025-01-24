b68a7a706d89c9d923a9cad296df7bff41b0e7dc
zhanerke <zhanerke0613@gmail.com>
2025-01-24 14:20:21 +0500

    dictionaries

---
+++ lab2/dictionaries/1.py
@@ -0,0 +1,45 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#Print the number of items in the dictionary:
+print(len(thisdict))
+
+
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#type
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+
+#Using the dict() method to make a dictionary:
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
