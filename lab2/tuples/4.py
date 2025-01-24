7168e6ba820bc5b94db6fa994fa487d93539a5c0
zhanerke <zhanerke0613@gmail.com>
2025-01-24 12:20:45 +0500

    unpack list

---
+++ lab2/tuples/4.py
@@ -0,0 +1,25 @@
+#Unpacking a Tuple
+
+fruits = ("apple", "banana", "cherry")
+
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
