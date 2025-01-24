8cc7a75e873b58b6c898004781abffb347a38414
zhanerke <zhanerke0613@gmail.com>
2025-01-24 11:57:21 +0500

    copy list

---
+++ lab2/lists/9.py
@@ -0,0 +1,14 @@
+#Copy lists
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
