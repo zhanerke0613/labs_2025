95db84fedf4646c361eb130d0baa2e9b9622b10c
zhanerke <zhanerke0613@gmail.com>
2025-01-24 11:50:23 +0500

    list comprehension

--- lab2/lists/7.py
+++ lab2/lists/7.py
@@ -1,4 +1,4 @@
-#List Comprehension
+#list comprehension
 
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = []
@@ -11,30 +11,20 @@ print(newlist)
 
 
 fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
 newlist = [x for x in fruits if "a" in x]
 
 print(newlist)
 
 
-
-newlist = [expression for item in iterable if condition == True]
-
-
 newlist = [x for x in fruits if x != "apple"]
-
 
 newlist = [x for x in fruits]
 
-
 newlist = [x for x in range(10)]
 
-
 newlist = [x for x in range(10) if x < 5]
-
 
 newlist = [x.upper() for x in fruits]
 
 newlist = ['hello' for x in fruits]
-
-
-newlist = [x if x != "banana" else "orange" for x in fruits]
