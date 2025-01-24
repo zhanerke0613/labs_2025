cfb7e88c342fd8910975c4df04f62ea6b6db107d
zhanerke <zhanerke0613@gmail.com>
2025-01-24 12:33:45 +0500

    access set items

---
+++ lab2/sets/2.py
@@ -0,0 +1,13 @@
+#Access Set Items
+
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" not in thisset)
