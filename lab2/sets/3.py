a6fe2627bcffc207cfd12824bdc6d26964329d53
zhanerke <zhanerke0613@gmail.com>
2025-01-24 12:35:12 +0500

    add set items

---
+++ lab2/sets/3.py
@@ -0,0 +1,17 @@
+#Add Set Items
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+thisset.update(tropical)
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+thisset.update(mylist)
+print(thisset)
