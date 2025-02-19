058cdc84d66d4616d6f943787fe21e19420701e8
zhanerke <zhanerke0613@gmail.com>
2025-02-19 23:47:31 +0500

    j

--- lab4/json/1.py
+++ lab4/json/1.py
@@ -1,9 +1,18 @@
 import json
-heading =  'Interface Status\n{}\n{:50} {:21} {:8} {:4}\n{} {}  {}  {}'.format('='*80, 'DM' ,'Description', 'Speed', 'MTU', '-'*50, '-'*20, '-'*6, '-'*6)
 
-with open('sample-data.json') as f:
-    main_data = json.load(f)
-    print(heading)
-    for i in main_data["imdata"]:
-        mn = i["l1PhysIf"]["attributes"]
-        print('{:50} {:20} {:8}  {}'.format(mn["dn"], mn["descr"], mn["speed"], mn["mtu"]))
+with open("sample-data.json", "r") as file:
+    data = json.load(file)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':<50}{'Description':<20}{'Speed':<8}{'MTU':<6}")
+print("-" * 50 + " " + "-" * 18 + " " + "-" * 6 + " " + "-" * 6)
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+    dn = attributes["dn"]
+    desc = attributes.get("descr", "")  
+    speed = attributes["speed"]
+    mtu = attributes["mtu"]
+
+    print(f"{dn:<50}{desc:<20}{speed:<8}{mtu:<6}")
